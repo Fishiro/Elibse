b572c924c580ba99c0c0e730a823d2cc515cf40b
Du Qui <nguyenduquicm1@gmail.com>
2026-01-10 10:31:14 +0700

    Add Violator form feature and link event to open that form

--- Admin/Violator.Designer.cs
+++ Admin/Violator.Designer.cs
@@ -28,110 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dgvViolators = new System.Windows.Forms.DataGridView();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.cboSort = new System.Windows.Forms.ComboBox();
+            this.btnUnlock = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViolators)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvViolators
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(954, 413);
-            this.dataGridView1.TabIndex = 1;
+            this.dgvViolators.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvViolators.Location = new System.Drawing.Point(12, 67);
+            this.dgvViolators.Name = "dgvViolators";
+            this.dgvViolators.RowHeadersWidth = 62;
+            this.dgvViolators.RowTemplate.Height = 28;
+            this.dgvViolators.Size = new System.Drawing.Size(954, 413);
+            this.dgvViolators.TabIndex = 1;
             // 
-            // button1
+            // btnReload
             // 
-            this.button1.Location = new System.Drawing.Point(452, 497);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 35);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Tải lại";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnReload.Location = new System.Drawing.Point(452, 497);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(75, 35);
+            this.btnReload.TabIndex = 8;
+            this.btnReload.Text = "Tải lại";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
             // 
-            // textBox1
+            // txtSearch
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(411, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(451, 35);
-            this.textBox1.TabIndex = 10;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(411, 13);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(451, 35);
+            this.txtSearch.TabIndex = 10;
             // 
-            // button2
+            // btnSearch
             // 
-            this.button2.Location = new System.Drawing.Point(876, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 36);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Tìm sách";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSearch.Location = new System.Drawing.Point(876, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 36);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "Tìm sách";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(231, 20);
+            this.label2.Location = new System.Drawing.Point(18, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 20);
             this.label2.TabIndex = 12;
             this.label2.Text = "Thứ tự";
             // 
-            // comboBox2
+            // cboSort
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
+            this.cboSort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSort.FormattingEnabled = true;
+            this.cboSort.Items.AddRange(new object[] {
             "A->Z",
             "Z->A"});
-            this.comboBox2.Location = new System.Drawing.Point(291, 17);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(73, 28);
-            this.comboBox2.TabIndex = 11;
+            this.cboSort.Location = new System.Drawing.Point(78, 17);
+            this.cboSort.Name = "cboSort";
+            this.cboSort.Size = new System.Drawing.Size(73, 28);
+            this.cboSort.TabIndex = 11;
+            this.cboSort.SelectedIndexChanged += new System.EventHandler(this.cboSort_SelectedIndexChanged);
             // 
-            // label1
+            // btnUnlock
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 20);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Bộ lọc";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Tên tác giả",
-            "Danh mục",
-            "Ngày mượn"});
-            this.comboBox1.Location = new System.Drawing.Point(78, 17);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 13;
+            this.btnUnlock.Location = new System.Drawing.Point(860, 497);
+            this.btnUnlock.Name = "btnUnlock";
+            this.btnUnlock.Size = new System.Drawing.Size(106, 35);
+            this.btnUnlock.TabIndex = 8;
+            this.btnUnlock.Text = "Mở khóa";
+            this.btnUnlock.UseVisualStyleBackColor = true;
+            this.btnUnlock.Click += new System.EventHandler(this.btnUnlock_Click);
             // 
             // Violator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 544);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cboSort);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUnlock);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.dgvViolators);
             this.Name = "Violator";
             this.Text = "Elibse: Độc Giả Vi Phạm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViolators)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,13 +130,12 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgvViolators;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cboSort;
+        private System.Windows.Forms.Button btnUnlock;
     }
 }
--- Admin/Violator.cs
+++ Admin/Violator.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Elibse
@@ -15,6 +10,165 @@ namespace Elibse
         public Violator()
         {
             InitializeComponent();
+        }
+
+        // --- SỰ KIỆN KHI MỞ FORM ---
+        private void Violator_Load(object sender, EventArgs e)
+        {
+            // 1. Cài đặt các lựa chọn cho ComboBox sắp xếp
+            // Lưu ý: Đảm bảo bạn đã đặt tên ComboBox là cboSort trong Design
+            if (cboSort.Items.Count == 0)
+            {
+                cboSort.Items.Add("Tên A->Z");
+                cboSort.Items.Add("Tên Z->A");
+                cboSort.Items.Add("Mới bị khóa gần đây");
+            }
+            cboSort.SelectedIndex = 0; // Mặc định chọn cái đầu tiên
+
+            LoadViolators("");
+            SetupDataGridView();
+        }
+
+        // --- 1. TẢI DANH SÁCH (CÓ SẮP XẾP) ---
+        private void LoadViolators(string keyword)
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+
+                    // Câu lệnh cơ bản
+                    string query = @"SELECT ReaderID, FullName, DOB, PhoneNumber, Email, Status, CreatedDate 
+                                     FROM READERS 
+                                     WHERE Status = 'Locked' 
+                                     AND (FullName LIKE @kw OR ReaderID LIKE @kw)";
+
+                    // --- XỬ LÝ LOGIC SẮP XẾP ---
+                    // Lấy giá trị đang được chọn trong ComboBox
+                    string sortOption = cboSort.SelectedItem?.ToString() ?? "";
+
+                    switch (sortOption)
+                    {
+                        case "Tên A->Z":
+                            query += " ORDER BY FullName ASC";
+                            break;
+                        case "Tên Z->A":
+                            query += " ORDER BY FullName DESC";
+                            break;
+                        case "Mới bị khóa gần đây":
+                            // Sắp xếp theo ngày tạo (hoặc ngày khóa nếu có) giảm dần
+                            query += " ORDER BY CreatedDate DESC";
+                            break;
+                        default:
+                            query += " ORDER BY FullName ASC";
+                            break;
+                    }
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@kw", "%" + keyword + "%");
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvViolators.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải dữ liệu: " + ex.Message);
+            }
+        }
+
+        // --- 2. ĐỊNH DẠNG BẢNG ---
+        private void SetupDataGridView()
+        {
+            dgvViolators.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            if (dgvViolators.Columns["ReaderID"] != null) dgvViolators.Columns["ReaderID"].HeaderText = "Mã Độc Giả";
+            if (dgvViolators.Columns["FullName"] != null) dgvViolators.Columns["FullName"].HeaderText = "Họ Tên";
+            if (dgvViolators.Columns["PhoneNumber"] != null) dgvViolators.Columns["PhoneNumber"].HeaderText = "SĐT";
+            if (dgvViolators.Columns["Email"] != null) dgvViolators.Columns["Email"].HeaderText = "Email";
+            if (dgvViolators.Columns["Status"] != null) dgvViolators.Columns["Status"].HeaderText = "Trạng Thái";
+
+            // Ẩn cột ngày tạo (chỉ dùng để sắp xếp, không cần hiện)
+            if (dgvViolators.Columns["CreatedDate"] != null) dgvViolators.Columns["CreatedDate"].Visible = false;
+            // Ẩn cột DOB nếu không cần thiết
+            if (dgvViolators.Columns["DOB"] != null) dgvViolators.Columns["DOB"].Visible = false;
+        }
+
+        // --- 3. CÁC SỰ KIỆN NÚT BẤM & TÌM KIẾM ---
+
+        // Nút Tìm kiếm
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadViolators(txtSearch.Text.Trim());
+        }
+
+        // Nhấn Enter tại ô tìm kiếm
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                LoadViolators(txtSearch.Text.Trim());
+                e.SuppressKeyPress = true; // Tắt tiếng bíp
+            }
+        }
+
+        // Nút Tải lại (Reset bộ lọc)
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+            cboSort.SelectedIndex = 0; // Reset về mặc định
+            LoadViolators("");
+        }
+
+        // --- SỰ KIỆN QUAN TRỌNG: KHI CHỌN SẮP XẾP KHÁC ---
+        // (Nhớ quay lại Design, chọn cboSort -> Tab Events -> Double click vào SelectedIndexChanged để gán hàm này)
+        private void cboSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Tải lại dữ liệu ngay lập tức khi người dùng chọn kiểu sắp xếp mới
+            LoadViolators(txtSearch.Text.Trim());
+        }
+
+        // --- 4. XỬ LÝ MỞ KHÓA (ÂN XÁ) ---
+        private void btnUnlock_Click(object sender, EventArgs e)
+        {
+            if (dgvViolators.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn độc giả cần mở khóa!", "Thông báo");
+                return;
+            }
+
+            // Lấy thông tin dòng đang chọn
+            string readerID = dgvViolators.SelectedRows[0].Cells["ReaderID"].Value.ToString();
+            string readerName = dgvViolators.SelectedRows[0].Cells["FullName"].Value.ToString();
+
+            if (MessageBox.Show($"Bạn có chắc muốn mở khóa cho độc giả {readerName}?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    using (SqlConnection conn = DatabaseConnection.GetConnection())
+                    {
+                        conn.Open();
+                        string sql = "UPDATE READERS SET Status = 'Active' WHERE ReaderID = @rid";
+                        SqlCommand cmd = new SqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("@rid", readerID);
+                        cmd.ExecuteNonQuery();
+
+                        // Ghi Log
+                        Logger.Log("Quản lý Độc giả", $"Đã mở khóa tài khoản cho độc giả: {readerName} ({readerID})");
+
+                        MessageBox.Show("Đã mở khóa thành công!");
+
+                        // Tải lại danh sách (độc giả vừa mở khóa sẽ biến mất khỏi danh sách vi phạm)
+                        LoadViolators(txtSearch.Text.Trim());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi: " + ex.Message);
+                }
+            }
         }
     }
 }
