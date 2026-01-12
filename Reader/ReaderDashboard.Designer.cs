9bcdaff0fa8e650a27fb484b92c4d0e7f00e2c0d
Du Qui <nguyenduquicm1@gmail.com>
2026-01-12 12:06:06 +0700

    Add ReaderDashboard form and event logic

---
+++ Reader/ReaderDashboard.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Elibse.Reader
+{
+    partial class ReaderDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.btnBorrow = new System.Windows.Forms.Button();
+            this.dgvMyBooks = new System.Windows.Forms.DataGridView();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblUserInfo = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyBooks)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(442, 399);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnBorrow);
+            this.tabPage1.Controls.Add(this.dgvBooks);
+            this.tabPage1.Controls.Add(this.btnSearch);
+            this.tabPage1.Controls.Add(this.txtSearch);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(434, 373);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tìm sách & Mượn";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnReturn);
+            this.tabPage2.Controls.Add(this.dgvMyBooks);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(434, 373);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tủ sách của tôi";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dgvHistory);
+            this.tabPage3.Controls.Add(this.groupBox1);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(434, 373);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Tài khoản";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(8, 20);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(337, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(351, 20);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(8, 59);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.Size = new System.Drawing.Size(420, 256);
+            this.dgvBooks.TabIndex = 2;
+            // 
+            // btnBorrow
+            // 
+            this.btnBorrow.Location = new System.Drawing.Point(158, 330);
+            this.btnBorrow.Name = "btnBorrow";
+            this.btnBorrow.Size = new System.Drawing.Size(121, 27);
+            this.btnBorrow.TabIndex = 3;
+            this.btnBorrow.Text = "Mượn sách này";
+            this.btnBorrow.UseVisualStyleBackColor = true;
+            this.btnBorrow.Click += new System.EventHandler(this.btnBorrow_Click);
+            // 
+            // dgvMyBooks
+            // 
+            this.dgvMyBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMyBooks.Location = new System.Drawing.Point(8, 6);
+            this.dgvMyBooks.Name = "dgvMyBooks";
+            this.dgvMyBooks.Size = new System.Drawing.Size(418, 304);
+            this.dgvMyBooks.TabIndex = 0;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(173, 324);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(89, 34);
+            this.btnReturn.TabIndex = 1;
+            this.btnReturn.Text = "Trả sách";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLogout);
+            this.groupBox1.Controls.Add(this.lblUserInfo);
+            this.groupBox1.Controls.Add(this.lblWelcome);
+            this.groupBox1.Location = new System.Drawing.Point(8, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(418, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin độc giả";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Location = new System.Drawing.Point(29, 38);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(58, 13);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Xin chào...";
+            // 
+            // lblUserInfo
+            // 
+            this.lblUserInfo.AutoSize = true;
+            this.lblUserInfo.Location = new System.Drawing.Point(29, 69);
+            this.lblUserInfo.Name = "lblUserInfo";
+            this.lblUserInfo.Size = new System.Drawing.Size(57, 13);
+            this.lblUserInfo.TabIndex = 0;
+            this.lblUserInfo.Text = "lblUserInfo";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(311, 49);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.TabIndex = 1;
+            this.btnLogout.Text = "Đăng xuất";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(8, 188);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.ReadOnly = true;
+            this.dgvHistory.Size = new System.Drawing.Size(418, 177);
+            this.dgvHistory.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lịch sử mượn sách";
+            // 
+            // ReaderDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 399);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "ReaderDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ReaderDashboard";
+            this.Load += new System.EventHandler(this.ReaderDashboard_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyBooks)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnBorrow;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.DataGridView dgvMyBooks;
+        private System.Windows.Forms.DataGridView dgvHistory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label lblUserInfo;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Reader/ReaderDashboard.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing; // Để dùng Color nếu cần
+using System.Windows.Forms;
+
+namespace Elibse.Reader
+{
+    public partial class ReaderDashboard : Form
+    {
+        // Biến lưu mã độc giả hiện tại
+        private string currentReaderID;
+
+        // Constructor nhận ID từ Login
+        public ReaderDashboard(string readerId)
+        {
+            InitializeComponent();
+            this.currentReaderID = readerId;
+        }
+
+        // Constructor mặc định (để tránh lỗi Designer)
+        public ReaderDashboard()
+        {
+            InitializeComponent();
+            this.currentReaderID = "R001"; // ID giả lập để test nếu chạy thẳng form này
+        }
+
+        // --- SỰ KIỆN KHI FORM LOAD ---
+        private void ReaderDashboard_Load(object sender, EventArgs e)
+        {
+            // Cấu hình giao diện bảng cho đẹp
+            dgvBooks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvMyBooks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Nếu có dgvHistory ở Tab 3 thì uncomment dòng dưới
+            // dgvHistory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Tải dữ liệu cho cả 3 tab
+            LoadAvailableBooks();
+            LoadMyBooks();
+            LoadUserInfoAndHistory();
+        }
+
+        // --- TAB 1: TÌM KIẾM & MƯỢN SÁCH ---
+
+        // Hàm load sách có sẵn
+        private void LoadAvailableBooks(string keyword = "")
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+                    string query = @"SELECT BookID AS [Mã Sách], Title AS [Tên Sách], 
+                                            Author AS [Tác Giả], Category AS [Thể Loại], Quantity AS [Còn Lại]
+                                     FROM BOOKS 
+                                     WHERE Quantity > 0 AND Status = 'Available'";
+
+                    if (!string.IsNullOrEmpty(keyword))
+                    {
+                        query += " AND (Title LIKE @kw OR Author LIKE @kw)";
+                    }
+
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    if (!string.IsNullOrEmpty(keyword))
+                        da.SelectCommand.Parameters.AddWithValue("@kw", "%" + keyword + "%");
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvBooks.DataSource = dt;
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Lỗi tải sách: " + ex.Message); }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadAvailableBooks(txtSearch.Text.Trim());
+        }
+
+        private void btnBorrow_Click(object sender, EventArgs e)
+        {
+            if (dgvBooks.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một cuốn sách để mượn!");
+                return;
+            }
+
+            string bookId = dgvBooks.SelectedRows[0].Cells["Mã Sách"].Value.ToString();
+
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+
+                    // 1. Kiểm tra xem độc giả có đang mượn cuốn này mà chưa trả không?
+                    string checkQuery = "SELECT COUNT(*) FROM LOAN_RECORDS WHERE BookID = @bid AND ReaderID = @rid AND ReturnDate IS NULL";
+                    SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+                    checkCmd.Parameters.AddWithValue("@bid", bookId);
+                    checkCmd.Parameters.AddWithValue("@rid", currentReaderID);
+
+                    if ((int)checkCmd.ExecuteScalar() > 0)
+                    {
+                        MessageBox.Show("Bạn đang mượn cuốn này rồi, không thể mượn thêm!");
+                        return;
+                    }
+
+                    // 2. Thực hiện mượn (Trừ số lượng sách + Thêm vào LoanRecords)
+                    // Dùng Transaction để đảm bảo cả 2 việc cùng thành công
+                    SqlTransaction transaction = conn.BeginTransaction();
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.Transaction = transaction;
+
+                    try
+                    {
+                        // Trừ số lượng sách
+                        cmd.CommandText = "UPDATE BOOKS SET Quantity = Quantity - 1 WHERE BookID = @BookID";
+                        cmd.Parameters.AddWithValue("@BookID", bookId);
+                        cmd.ExecuteNonQuery();
+
+                        // Thêm phiếu mượn (Hạn trả mặc định +7 ngày)
+                        cmd.CommandText = @"INSERT INTO LOAN_RECORDS (BookID, ReaderID, LoanDate, DueDate) 
+                                            VALUES (@BookID, @ReaderID, GETDATE(), GETDATE() + 7)";
+                        cmd.Parameters.AddWithValue("@ReaderID", currentReaderID);
+                        cmd.ExecuteNonQuery();
+
+                        transaction.Commit();
+                        MessageBox.Show("Mượn sách thành công! Hạn trả là 7 ngày.");
+
+                        // Refresh lại dữ liệu các tab
+                        LoadAvailableBooks();
+                        LoadMyBooks();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw; // Ném lỗi ra ngoài để catch bắt
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi mượn sách: " + ex.Message);
+            }
+        }
+
+        // --- TAB 2: TỦ SÁCH CỦA TÔI & TRẢ SÁCH ---
+
+        private void LoadMyBooks()
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+                    // Load danh sách đang mượn (ReturnDate IS NULL)
+                    string query = @"SELECT L.LoanID, B.BookID AS [Mã Sách], B.Title AS [Tên Sách], 
+                                            L.LoanDate AS [Ngày Mượn], L.DueDate AS [Hạn Trả]
+                                     FROM LOAN_RECORDS L
+                                     JOIN BOOKS B ON L.BookID = B.BookID
+                                     WHERE L.ReaderID = @rid AND L.ReturnDate IS NULL";
+
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    da.SelectCommand.Parameters.AddWithValue("@rid", currentReaderID);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvMyBooks.DataSource = dt;
+
+                    // Ẩn cột LoanID vì user không cần thấy
+                    if (dgvMyBooks.Columns["LoanID"] != null) dgvMyBooks.Columns["LoanID"].Visible = false;
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Lỗi tải tủ sách: " + ex.Message); }
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            if (dgvMyBooks.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn sách muốn trả!");
+                return;
+            }
+
+            // Lấy LoanID (đã ẩn) và BookID từ dòng được chọn
+            int loanId = Convert.ToInt32(dgvMyBooks.SelectedRows[0].Cells["LoanID"].Value);
+            string bookId = dgvMyBooks.SelectedRows[0].Cells["Mã Sách"].Value.ToString();
+
+            if (MessageBox.Show("Bạn có chắc muốn trả cuốn sách này?", "Xác nhận", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    using (SqlConnection conn = DatabaseConnection.GetConnection())
+                    {
+                        conn.Open();
+                        SqlTransaction transaction = conn.BeginTransaction();
+                        SqlCommand cmd = conn.CreateCommand();
+                        cmd.Transaction = transaction;
+
+                        try
+                        {
+                            // Cập nhật ngày trả
+                            cmd.CommandText = "UPDATE LOAN_RECORDS SET ReturnDate = GETDATE() WHERE LoanID = @lid";
+                            cmd.Parameters.AddWithValue("@lid", loanId);
+                            cmd.ExecuteNonQuery();
+
+                            // Cộng lại số lượng sách vào kho
+                            cmd.CommandText = "UPDATE BOOKS SET Quantity = Quantity + 1 WHERE BookID = @bid";
+                            cmd.Parameters.AddWithValue("@bid", bookId);
+                            cmd.ExecuteNonQuery();
+
+                            transaction.Commit();
+                            MessageBox.Show("Trả sách thành công!");
+
+                            // Refresh dữ liệu
+                            LoadMyBooks();
+                            LoadAvailableBooks();
+                            LoadUserInfoAndHistory();
+                        }
+                        catch
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi trả sách: " + ex.Message);
+                }
+            }
+        }
+
+        // --- TAB 3: TÀI KHOẢN & LỊCH SỬ ---
+
+        private void LoadUserInfoAndHistory()
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+
+                    // 1. Load Thông tin cá nhân
+                    string userQuery = "SELECT FullName, Email FROM READERS WHERE ReaderID = @rid";
+                    SqlCommand cmd = new SqlCommand(userQuery, conn);
+                    cmd.Parameters.AddWithValue("@rid", currentReaderID);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        // Tìm Label theo tên bạn đã đặt (nhớ kiểm tra đúng tên trong Designer)
+                        Control[] welcome = this.Controls.Find("lblWelcome", true);
+                        if (welcome.Length > 0) welcome[0].Text = "Xin chào, " + reader["FullName"].ToString();
+
+                        Control[] info = this.Controls.Find("lblUserInfo", true);
+                        if (info.Length > 0) info[0].Text = reader["Email"].ToString();
+                    }
+                    reader.Close();
+
+                    // 2. Load Lịch sử (Những sách đã trả) - Nếu bạn có dgvHistory
+                    Control[] historyGrid = this.Controls.Find("dgvHistory", true);
+                    if (historyGrid.Length > 0)
+                    {
+                        DataGridView dgvHist = (DataGridView)historyGrid[0];
+                        string histQuery = @"SELECT B.Title AS [Sách], L.LoanDate AS [Ngày Mượn], 
+                                                    L.ReturnDate AS [Ngày Trả]
+                                             FROM LOAN_RECORDS L
+                                             JOIN BOOKS B ON L.BookID = B.BookID
+                                             WHERE L.ReaderID = @rid AND L.ReturnDate IS NOT NULL
+                                             ORDER BY L.ReturnDate DESC";
+
+                        SqlDataAdapter da = new SqlDataAdapter(histQuery, conn);
+                        da.SelectCommand.Parameters.AddWithValue("@rid", currentReaderID);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        dgvHist.DataSource = dt;
+                    }
+                }
+            }
+            catch { /* Có thể bỏ qua lỗi hiển thị lịch sử nếu chưa cần gấp */ }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Đóng form dashboard để quay về login (nếu login gọi ShowDialog)
+        }
+    }
+}
