85b3e9ac429214f3172ca57ee5279cffca91e8ae
Du Qui <nguyenduquicm1@gmail.com>
2026-01-13 00:38:31 +0700

    Upgrade MD5 to SHA256

--- SecurityHelper.cs
+++ SecurityHelper.cs
@@ -6,21 +6,26 @@ namespace Elibse
 {
     public static class SecurityHelper
     {
-        // Hàm băm mật khẩu sang chuỗi MD5 (32 ký tự)
+        // Thêm Salt cứng để chống lại Rainbow Table cơ bản
+        private static readonly string _fixedSalt = "Salt_Elibse_Project_2025_SecretKey_!@#MOTIVATEDBY_NGOCNGAN";
+
         public static string HashPassword(string password)
         {
             if (string.IsNullOrEmpty(password)) return "";
 
-            using (MD5 md5 = MD5.Create())
-            {
-                byte[] inputBytes = Encoding.ASCII.GetBytes(password);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
+            // Nối password với chuỗi bí mật trước khi băm
+            string passwordWithSalt = password + _fixedSalt;
 
-                // Chuyển mảng byte thành chuỗi Hex
+            // Dùng SHA256
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(passwordWithSalt);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
                 StringBuilder sb = new StringBuilder();
                 for (int i = 0; i < hashBytes.Length; i++)
                 {
-                    sb.Append(hashBytes[i].ToString("X2")); // X2 để ra chữ hoa
+                    sb.Append(hashBytes[i].ToString("X2"));
                 }
                 return sb.ToString();
             }
